9aae65264d7b10ce8c43a6b10e80b01b46f7a8e9
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-03-19 20:42:21 +0530

    Geometry.py

---
+++ geometry.py
@@ -0,0 +1,32 @@
+#volume of the cone
+r=9.6
+h=4
+pi=22/7
+volume=(1/3)*pi*(r**2)*h
+area=int(volume)
+print("volume of the cone=",area)
+
+#area of the circle
+r=4.2
+pi=22/7
+area=(22/7)*(r**2)
+print("area of the circle is ",area)
+print(type(area))
+
+
+n=5
+p=6
+m=5*12
+r=13
+s=13/100
+t=(n*m*s)
+u=float(t)
+print("output of npr=",t)
+print(type(u))
+
+
+r=4.6
+pi=22/7
+circum=2*(22/7)*r
+print("circumfereance of the circle is",circum)
+print(type(circum))
