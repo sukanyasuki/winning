29e7263fd796c38f51773917ce5bb46fe20367b2
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-03-19 14:47:06 +0530

    sukanyasui.py

---
+++ sukanyasuki.py
@@ -0,0 +1,67 @@
+#int to float
+a=9
+b=float(a)
+print(type(a))
+print(type(b))
+
+#float to int
+a=4.3
+b=int(a)
+print(type(a))
+print(type(b))
+
+
+#int to string
+a=5
+b=str(a)
+print(type(a))
+print(type(b))
+
+
+#string to Bool
+a="Hello"
+b=bool(a)
+print(type(a))
+print(type(b))
+
+
+#Bool to string
+a=true
+b=str(a)
+print(type(a))
+print(type(b))
+
+
+#Bool to int
+a=true
+b=int(a)
+print(type(a))
+print(Type(b))
+
+#Bool to float
+a=true
+b=float(a)
+print(type(a))
+print(type(b))
+
+#int to bool
+a=7
+b=bool(a)
+print(type(a))
+print(type(b))
+
+
+#string to int"value Error: invalid literal for int()with base 10: "hello"
+'''a="hello"
+b=int(a)
+print(type(a))
+print(type(b))'''
+
+
+#string to float "value Error: could not convert string to float:'hello'"
+'''a="hello"
+b=float(a)
+print(type(a))
+print(type(b))'''
+
+
