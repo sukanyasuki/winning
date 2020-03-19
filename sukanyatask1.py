0a4a53b964883b87ee15d21323a3ed11674b4cc8
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-03-19 20:44:26 +0530

    sukanyatask1

---
+++ sukanyatask1.py
@@ -0,0 +1,65 @@
+#Difference between python2 and python3
+python2:
+   * If you print any function you cannot use parantheses.
+    Ex: print"Hello"
+   * You can divide the two integer values you will get output  nearest whole number.
+   Ex: 5/2=2
+   *This syntax is difficult to understend.
+   *We can iterate the function you used to Xrange() function.
+   *to store unicode string value you require to define them with "u".
+
+python3 :
+    * If you print any function you necessary use parantheses .
+     Ex : print("Hello")
+    * you can divide two integers values you will get a float value.
+     Ex : 5/2=2.5
+    * This syntax is simple and easy to understand.
+    * We can iterate the function  the new Range() function is used.
+    * Default storing of string is unicode.
+
+
+#Difference between compiler and interpreter
+ Compiler      * A compiler is a translator which transform high level language into machine language.
+     * In program compilation done before execution.
+     * The speed of compiler is faster.
+     * In compiler error detection is difficult.
+     * Compiler display all errors after compilation of a program.
+ Interpreter :
+     * An interpreter is a program which imitates the execution of programs written in asource language.
+     * An interpreterboth compilation and execution takes place simultaneously.
+     * The speed of interpreter is slower.
+     * An interpreter  error detection is easier.
+     * An interpreter display errors line one by one.
+
+     
+# What's Escape sequence
+
+
+     * An escape sequence is a combination of characters that has a special meaning. 
+     * In python Escape sequence are denoted by a backslash (\).
+     * The most important one may be \n indicates a new line.
+     * Another escape sequence wroth mentioning is \' for a single quote whithin a string.
+     
+Examples :
+    1. \newline =Blackslash and newline ignored
+     print("line1\line2\line3")
+     2. \\ =Blackslash(\)
+         print("\\")
+    3. \' =single quote(')
+         print('\'')
+    4. \" =Double quote(")
+         print("\"")
+
+
+
+# Good features of python programming language
+                        
+    * Python is high level,free open source programming language.
+    * Python supports object orientedprogramming as well as procedural oriented programming.
+    * python is easy to learn.
+    * In python we dont need to declare the type of variable because it is a dynamic programming language.
+      Ex : x=12 it means x is a integer or string                  
+     * Python is a extensible language.we can write code in c or c++ languages we can compile that code in c or c++ languages.
+    * we can run python program in any platform.                    
+     * Python has a large standard library it provides rich set of modules and functions.
+
