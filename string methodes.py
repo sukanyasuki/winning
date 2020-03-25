f3a1dfffab7fe6287803b82723482791977434fe
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-03-25 18:54:50 +0530

    string methods

---
+++ string methodes.py
@@ -0,0 +1,34 @@
+#task 1
+a=input()
+b=(a[0]+a[1:2].upper()+a[2:4]+a[4:5].upper()+a[5:])
+print(b)
+
+
+#task  2
+a=input()
+b=int(input())
+c=str(b)
+e=len(a)
+d=(a*b+c*e)
+print(d)
+
+
+#task  3
+a=input()
+b=(a[0].upper()+a[1:3]+a[3:4].upper()+a[4:5]+a[5:].upper())
+print(b)
+
+
+#task  4
+a=input()
+b=input()
+c=a+b
+d=(c[0].upper()+c[1:5]+c[5:6].upper()+c[6:7].upper()+c[7:9]+c[9:].upper())
+print(d)
+
+
+#task  5
+a="3"
+b="5"
+c=(a*5+b*3)
+print(c)
