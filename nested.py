446a3f29c2811826f69e9df8cafe36c6feb72b51
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-04-01 21:55:27 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,13 @@
+list=[[1,2,3],4,5,6,[8,9],[[44,55,66],[66,77],["hello","python","welcome"]]]
+print(list[0][-1])
+print(list[2])
+
+print(list[4][-1])
+print(list[5][1][-1])
+print(list[5][0][1])
+print(list[5][1][-1])
+print(list[5][0][0])
+print(list[5][2][-1])      
+print(list[5][2][2][3:])
+print(list[-1][-1][-2]+" "+list[-1][-1][-1])
+print(list[5][2][0][2:])
