7cbc72d93793434902a8e3dde7145bdfcccdc5f6
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-04-03 20:09:49 +0530

    Add files via upload

---
+++ create empty list.py
@@ -0,0 +1,15 @@
+#create an empty list
+a=[]
+print(a)
+#create an empty tuple
+a=()
+print(a)
+#create a empty set
+a={}
+print(a)
+#create a empty dictionary
+a={}
+print(a)
+#using dict () we can create a empty dictionary
+b=dict()
+print(b)
