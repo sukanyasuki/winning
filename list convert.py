5d3f955bc9eaa45f51e15cb7f1090849ea439674
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-04-03 20:10:29 +0530

    Add files via upload

---
+++ list convert.py
@@ -0,0 +1,40 @@
+#convert tuple to list
+a=(1,2,3,4,5)
+b=list(a)
+print(b)
+#convert list to tuple
+a=[2,4,7,8,9]
+b=tuple(a)
+print(b)
+#convert list to set
+a=[67,89,45,3,2]
+b=set(a)
+print(b)
+#convert set to tuple
+a={23,45,"hello",2}
+b=tuple(a)
+print(b)
+#convert set to list
+a={"hello","python",45,9,10}
+b=list(a)
+print(b)
+#convert tuple to set
+a=(4,7,8,2,6)
+b=set(a)
+print(b)
+#tuple count method
+a=(4,5,6,4,3,5,8,4,4)
+b=a.count(4)
+c=a.count(5)
+print(b)
+print(c)
+#tuple index method
+a=(1,2,3,4,5)
+b=a.index(3)
+print(b)
+
+
+
+
+
+
