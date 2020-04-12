30af34ecadf725b79b708921d90aaf6272a9841c
sukanyasuki <62152672+sukanyasuki@users.noreply.github.com>
2020-04-12 07:54:45 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,46 @@
+#print vowels and without vowels
+
+'''list=["hello","bcd","rty","hmm","outr","python"]
+vowels=[]
+withoutvowels=[]
+for i in  list:
+    if ('a'in i or 'e'in i or 'i'in i or 'o'in i or'u'in i):
+        vowels.append(i)
+    else:
+        withoutvowels.append(i)
+print("list with vowels",vowels)
+    
+print("list withoutvowels",withoutvowels)'''
+
+#list start with h
+
+'''
+a=["hello","hemm","kj","fer","hi","python"]
+h=[]
+for i in a:
+    if(i[0]=='h'):
+        h.append(i)
+
+print("list start with h",h)'''
+
+
+#list end with d
+
+'''
+a=["python","erfd","dad","hi","end"]
+d=[]
+for i in a:
+    if(i[-1]=='d'):
+        d.append(i)
+print("list end with d",d)'''
+
+
+
+
+
+
+
+
+
+
+
